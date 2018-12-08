99b183b6cbd0cbf675de2727297dacf3741bcdb5
fimashagal <fimashagal@gmail.com>
2018-12-08 20:15:50 +0200

    init

--- sw.js
+++ sw.js
@@ -3,8 +3,7 @@ importScripts("./js/sw-toolbox.js");
 toolbox.precache([
     "index.html",
     "css/index.css",
-    "js/localforage.min.js",
-    "js/phaser.min.js"
+    "js/glide.min.js"
 ]);
 toolbox.router.get("/assets/*", toolbox.cacheFirst);
 toolbox.router.get("/*", toolbox.networkFirst, {
